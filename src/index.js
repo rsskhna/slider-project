c6d35ce2c86f50ec1d0190bdf2e2461045a1e9cf
Irina Rossokhina <i.rossokhina@yandex.ru>
2023-11-30 17:42:01 +0300

    popup is implemented

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import './index.css';
+import {openModal, closeModal} from "./scripts/modal";
+
+const popup = document.querySelector('.popup');
+const buttonMore = document.querySelector('.slide-three__button');
+const dots = document.querySelectorAll('.popup__dot');
+const pointLists = document.querySelectorAll('.popup__point-list');
+
+
+buttonMore.addEventListener('click', function () {
+    openModal(popup);
+});
+
+popup.addEventListener('click', function (evt) {
+    if (evt.target.classList.contains('popup__close')) {
+        closeModal(popup);
+    }
+
+    if (evt.target.classList.contains('popup__arrow')) {
+        dots.forEach((dot) => {
+            dot.classList.toggle('popup__dot_inactive');
+        });
+        pointLists.forEach((list) => {
+           list.classList.toggle('popup__point-list_inactive');
+        });
+    }
+})
+
---
+++ src/scripts/modal.js
@@ -0,0 +1,14 @@
+export {openModal, closeModal}
+
+function openModal(modal) {
+    modal.classList.add('popup_is-opened');
+}
+
+function closeModal(modal) {
+    modal.classList.remove('popup_is-opened');
+}
+
+
+
+
+
